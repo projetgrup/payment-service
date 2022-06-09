d7689fc28671e1a90ed3211f78a16b0ae764691b
hasan.tayfur <hasant@projetgrup.com>
2022-06-09 23:58:21 +0300

    [IMP] new module 'system' and payment system integrations

--- payment_vendor/controllers/main.py
+++ payment_vendor/controllers/main.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
-from odoo.http import request
+from odoo.http import route, request
 from odoo.addons.payment_jetcheckout_system.controllers.main import JetcheckoutSystemController as JetSystemController
+from odoo.addons.portal.controllers import portal
 
-
-class VendorPaymentController(JetSystemController):
-
-    def _jetcheckout_tx_vals(self, **kwargs):
-        res = super()._jetcheckout_tx_vals(**kwargs)
+class CustomerPortal(portal.CustomerPortal):
+    @route(['/my', '/my/home'], type='http', auth="user", website=True)
+    def home(self, **kwargs):
         system = kwargs.get('system', request.env.company.system)
         if system == 'vendor':
-            pass
-        return res
+            return request.redirect('/my/payment')
+        return super().home(**kwargs)
 
-    def _jetcheckout_system_page_values(self, company, system, parent, transaction):
-        res = super()._jetcheckout_system_page_values(company, system, parent, transaction)
-        if system == 'vendor':
-            pass
-        return res
+#class VendorPaymentController(JetSystemController):
+
+    #def _jetcheckout_tx_vals(self, **kwargs):
+    #    res = super()._jetcheckout_tx_vals(**kwargs)
+    #    system = kwargs.get('system', request.env.company.system)
+    #    if system == 'vendor':
+    #        pass
+    #    return res
+
+    #def _jetcheckout_system_page_values(self, company, system, parent, transaction):
+    #    res = super()._jetcheckout_system_page_values(company, system, parent, transaction)
+    #    if system == 'vendor':
+    #        pass
+    #    return res
