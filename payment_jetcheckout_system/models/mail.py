107d91b811b2ce6e22c046f0e5c83ce27f7388b7
hasan.tayfur <hasant@projetgrup.com>
2022-06-15 00:00:45 +0300

    [FIX] email sending issue fixed

--- payment_jetcheckout_system/models/mail.py
+++ payment_jetcheckout_system/models/mail.py
@@ -42,30 +42,3 @@ class MailServer(models.Model):
         if self.env.company.system:
             res['company_id'] = self.env.company.id
         return res
-
-
-class Mailing(models.Model):
-    _inherit = 'mailing.mailing'
-
-    @api.depends('mailing_domain')
-    def _compute_partner_ids(self):
-        for mail in self:
-            mail.partner_ids = self.env['res.partner'].sudo().search(literal_eval(self.mailing_domain))
-
-    company_id = fields.Many2one('res.company')
-    partner_ids = fields.Many2many('res.partner', compute='_compute_partner_ids', store=True)
-
-    @api.model
-    def default_get(self, fields):
-        res = super(Mailing, self).default_get(fields)
-        res['company_id'] = self.env.company.id
-        return res
-
-
-class UtmSource(models.Model):
-    _inherit = 'utm.source'
-
-    def write(self, vals):
-        if self.env.context.get('readonly'):
-            return
-        return super().write(vals)
