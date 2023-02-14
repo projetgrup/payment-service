29233c277da35ac2d1e3ad5c69d565249c4eaa07
Hasan Tayfur <hasant@projetgrup.com>
2023-02-14 10:40:06 +0300

    [REM] pos modules are removed

--- pos_sync/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright © 2022 Projetgrup (https://www.bulutkobi.io)
-# Part of Projetgrup BulutKOBI. See LICENSE file for full copyright and licensing details.
-
-from . import main
