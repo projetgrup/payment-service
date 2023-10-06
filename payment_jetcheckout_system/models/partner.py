0e482fc8b98b99e530a6d4d1144b2fdd96cd24e5
hasan.tayfur <fi3ei2@gmail.com>
2023-10-06 15:11:34 +0300

    [IMP] partner tags has been added to vendor payment page

--- payment_jetcheckout_system/models/partner.py
+++ payment_jetcheckout_system/models/partner.py
@@ -199,7 +199,7 @@ class Partner(models.Model):
         return self.search([('vat', '!=', False), ('vat', '=', self.vat)]).mapped('company_id')
 
     def _get_tags(self):
-        return self.search([('vat', '!=', False), ('vat', '=', self.vat)]).mapped('category_id')
+        return self.search([('vat', '!=', False), ('vat', '=', self.vat), ('company_id', '=', self.env.company.id)]).mapped('category_id')
 
     @api.model
     def _resolve_token(self, token):
