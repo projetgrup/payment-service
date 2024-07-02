6e8bddcb3f0c607707e2cce7b5d485ba0b6da68d
hasan.tayfur <fi3ei2@gmail.com>
2024-07-02 20:01:41 +0300

    [IMP] iban service added

--- payment_jetcheckout_system/static/src/js/menu.js
+++ payment_jetcheckout_system/static/src/js/menu.js
@@ -5,19 +5,22 @@ import { patch } from "web.utils";
 
 patch(WebClient.prototype, "payment_jetcheckout_system.menu_main", {
     async loadRouterState() {
+        const _super = this._super;
         const system = this.user.context.system;
         if (system) {
-            const controller = this.actionService.currentController;
-            if (controller) {
+            let state = await this.actionService.loadState();
+            let menuID = Number(this.router.current.hash.menu_id || 0);
+            if (state && !menuID) {
+                const controller = this.actionService.currentController;
                 const actionId = controller && controller.action.id;
                 const menu = this.menuService.getAll().find((m) => m.actionID === actionId && m.xmlid === `payment_${system}.menu_main`);
-                const menuID = menu && menu.appID;
+                menuID = menu && menu.appID;
                 if (menuID) {
                     this.menuService.setCurrentMenu(menuID);
                     return;
                 }
             }
         }
-        return this._super.apply(this, arguments);
+        return _super.apply(this, arguments);
     },
 });
--- payment_syncops/models/settings.py
+++ payment_syncops/models/settings.py
@@ -6,11 +6,12 @@ class PaymentSettings(models.TransientModel):
     _inherit = 'payment.settings'
 
     syncops_sync_item_force = fields.Boolean(related='company_id.syncops_sync_item_force', readonly=False)
-    syncops_cron_sync_partner = fields.Boolean(related='company_id.syncops_cron_sync_partner', readonly=False)
-    syncops_payment_page_partner_required = fields.Boolean(related='company_id.syncops_payment_page_partner_required', readonly=False)
     syncops_cron_sync_item = fields.Boolean(related='company_id.syncops_cron_sync_item', readonly=False)
-    syncops_cron_sync_item_subtype = fields.Selection(related='company_id.syncops_cron_sync_item_subtype', readonly=False)
+    syncops_cron_sync_partner = fields.Boolean(related='company_id.syncops_cron_sync_partner', readonly=False)
     syncops_cron_sync_item_hour = fields.Integer(related='company_id.syncops_cron_sync_item_hour', readonly=False)
+    syncops_cron_sync_item_subtype = fields.Selection(related='company_id.syncops_cron_sync_item_subtype', readonly=False)
+    syncops_payment_page_partner_required = fields.Boolean(related='company_id.syncops_payment_page_partner_required', readonly=False)
+    syncops_check_iban = fields.Boolean(related='company_id.syncops_check_iban', readonly=False)
 
     @api.onchange('syncops_cron_sync_item_hour')
     def onchange_syncops_cron_sync_item_hour(self):
