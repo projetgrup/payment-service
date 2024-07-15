56259aedd0388104a458e834fe6dc14b2f148345
hasan.tayfur <fi3ei2@gmail.com>
2024-07-15 11:06:28 +0300

    [IMP] payment order checkout module

--- payment_oco_api/services/services.py
+++ payment_oco_api/services/services.py
@@ -2,12 +2,12 @@
 import base64
 import hashlib
 import logging
+from urllib.parse import quote
 
-from odoo import _
-from odoo.http import Response
+from odoo.http import Response, request
+from odoo.tools.translate import _, _lt
 from odoo.addons.base_rest import restapi
 from odoo.addons.base_rest_datamodel.restapi import Datamodel
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 from odoo.addons.component.core import Component
 
 _logger = logging.getLogger(__name__)
@@ -25,68 +25,6 @@ class OrderCheckoutAPIService(Component):
     _description = """This API helps you connect oco payment system with your specially generated key"""
 
     @restapi.method(
-        [(["/payment/status"], "GET")],
-        input_param=Datamodel("oco.payment.read.input"),
-        output_param=Datamodel("oco.payment.read.output"),
-        auth="public",
-        tags=['Payments']
-    )
-    def get_payments(self, params):
-        """
-        Get Payments
-        """
-        try:
-            api = self._get_api(params.apikey)
-            if not api:
-                return Response("Application key is not matched", status=401, mimetype="application/json")
-
-            company = api.company_id
-            payments = self._get_payment(company, params.reference)
-            if not payments:
-                return Response("Payment not found", status=404, mimetype="application/json")
-            
-            results = [{
-                "date": payment.create_date.strftime(DTF),
-                "reference": payment.jetcheckout_order_id or '',
-                "status": payment.state or '',
-                "description": payment.state_message or '',
-                "payment_payable": payment.jetcheckout_payment_amount or 0,
-                "customer_rate": payment.jetcheckout_customer_rate or 0,
-                "customer_amount": payment.jetcheckout_customer_amount or 0,
-                "payment_paid": payment.jetcheckout_payment_paid or 0,
-                "commission_rate": payment.jetcheckout_commission_rate or 0,
-                "commission_amount": payment.jetcheckout_commission_amount or 0,
-                "payment_net": payment.jetcheckout_payment_net or 0,
-                "fund_amount": payment.jetcheckout_fund_amount or 0,
-                "fund_rate": payment.jetcheckout_fund_rate or 0,
-                "prepayment_amount": payment.paylox_prepayment_amount or 0,
-                "installment_amount": payment.jetcheckout_installment_amount or 0,
-                "installment_count": payment.jetcheckout_installment_count or 1,
-                "installment_plus": payment.jetcheckout_installment_plus or 0,
-                "installment_description": payment.jetcheckout_installment_description or '',
-                "card_name": payment.jetcheckout_card_name or '',
-                "card_number": payment.jetcheckout_card_number or '',
-                "card_type": payment.jetcheckout_card_type or '',
-                "card_family": payment.jetcheckout_card_family or '',
-                "campaign_name": payment.jetcheckout_campaign_name or '',
-                "pos_name": payment.jetcheckout_vpos_name or '',
-                "transaction_id": payment.jetcheckout_transaction_id or '',
-                "transaction_name": payment.reference or '',
-                "partner_name": payment.partner_name or '',
-                "partner_email": payment.partner_email or '',
-                "partner_phone": payment.partner_phone or '',
-                "partner_address": payment.partner_address or '',
-                "ip_address": payment.jetcheckout_ip_address or '',
-                "url_address": payment.jetcheckout_url_address or '',
-            } for payment in payments]
-
-            ResponseOk = self.env.datamodels["oco.payment.read.output"]
-            return ResponseOk(payments=results, **RESPONSE[200])
-        except Exception as e:
-            _logger.error(e)
-            return Response("Server Error", status=500, mimetype="application/json")
-
-    @restapi.method(
         [(["/payment/create"], "POST")],
         input_param=Datamodel("oco.payment.create"),
         output_param=Datamodel("oco.payment.output"),
@@ -98,152 +36,32 @@ class OrderCheckoutAPIService(Component):
         Create Payments
         """
         try:
-            api = self._get_api(params.apikey)
+            company = self.env.company.id
+
+            api = self._get_api(company, params.apikey)
             if not api:
                 return Response("Application key is not matched", status=401, mimetype="application/json")
 
-            hash = self._get_hash(api, params.hash, 0)
+            hash = self._get_hash(api, params.hash, params.id)
             if not hash:
                 return Response("Hash is not matched", status=401, mimetype="application/json")
 
-            result = []
-            company = api.company_id
-            ocos = self.env['res.partner'].sudo()
-            for item in params.items:
-                oco = self._get_oco(company, item.oco)
-                if not oco:
-                    oco = self._create_oco(company, item.oco)
-                result.append({
-                    'vat': oco.vat,
-                    'link': oco._get_payment_url(shorten=True),
-                })
-                ocos |= oco
+            self._create_transaction(api, hash, params)
 
-                items = []
-                for payment in item.payments:
-                    items.append(self._prepare_item(company, oco, payment))
-                self._create_items(items)
-
-            types = []
-            vals = {}
-            
-
-            types = []
-            vals = {}
-            send = True
-            if company.api_item_notif_mail_create_ok:
-                template = company.api_item_notif_mail_create_template
-                if template:
-                    if company.api_item_notif_mail_create_filter_email:
-                        parent_email = parent.email
-                        emails = company.api_item_notif_mail_create_filter_email.split('\n')
-                        if company.api_item_notif_mail_create_filter_email_ok and all(email not in parent_email for email in emails):
-                            send = False
-                        elif not company.api_item_notif_mail_create_filter_email_ok and any(email in parent_email for email in emails):
-                            send = False
-                    types.append(self.env.ref('payment_jetcheckout_system.send_type_email').id)
-                    vals.update({'mail_template_id': template.id})
-            if company.api_item_notif_sms_create_ok:
-                template = company.api_item_notif_sms_create_template
-                if template:
-                    if company.api_item_notif_sms_create_filter_number:
-                        parent_number = parent.mobile.replace(' ', '')
-                        numbers = company.api_item_notif_sms_create_filter_number.split('\n')
-                        if company.api_item_notif_sms_create_filter_number_ok and all(number not in parent_number for number in numbers):
-                            send = False
-                        elif not company.api_item_notif_sms_create_filter_number_ok and any(number in parent_number for number in numbers):
-                            send = False
-                    types.append(self.env.ref('payment_jetcheckout_system.send_type_sms').id)
-                    vals.update({'sms_template_id': template.id})
-            if send and types:
-                authorized = self.env.ref('payment_jetcheckout_system.categ_authorized')
-                user = self.env['res.users'].sudo().search([
-                    ('company_id', '=', company.id),
-                    ('partner_id.category_id', 'in', [authorized.id])
-                ], limit=1) or self.env.user
-                sending = self.env['payment.acquirer.jetcheckout.send'].sudo().with_context(partners=parent).create({
-                    'selection': [(6, 0, types)],
-                    'type_ids': [(6, 0, types)],
-                    'company_id': company.id,
-                    **vals
-                })
-                sending.with_user(user).send()
-
-            ResponseOk = self.env.datamodels["oco.payment.output"]
-            return ResponseOk(result=result, **RESPONSE[200])
+            ResponseOk = self.env.datamodels["payment.prepare.output"]
+            return ResponseOk(hash=quote(hash), **RESPONSE[200])
         except Exception as e:
             _logger.error(e)
-            return Response("Server Error", status=500, mimetype="application/json")
+            return Response(str(e), status=500, mimetype="application/json")
 
-    @restapi.method(
-        [(["/campaign/update"], "PUT")],
-        input_param=Datamodel("oco.campaign.update"),
-        output_param=Datamodel("oco.campaign.output"),
-        auth="public",
-        tags=['Campaigns']
-    )
-    def update_campaign(self, params):
-        """
-        Update Campaigns
-        """
-        try:
-            api = self._get_api(params.apikey)
-            if not api:
-                return Response("Application key is not matched", status=401, mimetype="application/json")
-
-            hash = self._get_hash(api, params.hash, 0)
-            if not hash:
-                return Response("Hash is not matched", status=401, mimetype="application/json")
-
-            company = api.company_id
-            acquirer = self._get_acquirer(company)
-            for campaign in params.campaigns:
-                vat, ref = None, None
-                if hasattr(campaign.partner, 'vat') and campaign.partner.vat:
-                    vat = campaign.partner.vat
-                if hasattr(campaign.partner, 'ref') and campaign.partner.ref:
-                    ref = campaign.partner.ref
-                if not vat and not ref:
-                    return Response("One of VAT or Reference information must be sent with campaign name", status=400, mimetype="application/json")
-
-                oco = self._get_oco(company, campaign.partner)
-                if not oco:
-                    if vat:
-                        postfix = 'VAT %s' % vat
-                    elif ref:
-                        postfix = 'Reference %s' % ref
-                    else:
-                        postfix = 'given pairs'
-                    return Response("Partner cannot be found with %s" % postfix, status=404, mimetype="application/json")
-
-                campaign = self._get_campaign(acquirer, campaign.name)
-                if not campaign:
-                    return Response("Campaign name cannot be found for partner %s" % oco.name, status=404, mimetype="application/json")
-
-                oco.campaign_id = campaign.id
-
-            ResponseOk = self.env.datamodels["oco.campaign.output"]
-            return ResponseOk(**RESPONSE[200])
-        except Exception as e:
-            _logger.error(e)
-            return Response("Server Error", status=500, mimetype="application/json")
-
-    @restapi.webhook(
-        input_param=Datamodel("oco.payment.item.webhook"),
-        tags=['Payments']
-    )
-    def webhook_successful_payment(self):
-        """
-        Notify Successful Payment
-        """
-        pass
+    create_payments.__doc__ = _lt("Prepare Payment")
 
     #
     # PRIVATE METHODS
     #
 
-    def _get_api(self, apikey, secretkey=False):
-        domain = [('api_key', '=', apikey)]
+    def _get_api(self, company, apikey, secretkey=False):
+        domain = [('company_id', '=', company), ('api_key', '=', apikey)]
         if secretkey:
             domain.append(('secret_key', '=', secretkey))
         return self.env['payment.acquirer.jetcheckout.api'].sudo().search(domain, limit=1)
@@ -254,8 +72,134 @@ class OrderCheckoutAPIService(Component):
             return False
         return hash
 
+    def _create_transaction(self, api, hash, params):
+        if hasattr(params.partner, 'country'):
+            country = self.env['res.country'].sudo().search([('code', '=', params.partner.country)], limit=1)
+        else:
+            country = False
+
+        if country and hasattr(params.partner, 'state'):
+            state = self.env['res.country.state'].sudo().search([('country_id', '=', country.id), ('code', '=', params.partner.state)], limit=1)
+        else:
+            state = False
+
+        codes = hasattr(params, 'methods') and params.methods or []
+        method = ''
+        providers = []
+        for code in codes:
+            if code == 'bank':
+                providers.append('transfer')
+            elif code == 'card':
+                providers.append('jetcheckout')
+        if len(codes) == 1:
+            method = codes[0]
+
+        company = api.company_id
+        if hasattr(params, 'company'):
+            company = self.env['res.company'].sudo().search([('vat', '=', params.company.vat), ('parent_id', '=', company.id)])
+            if not company:
+                raise Exception('Company cannot be found')
+
+        partner = api.partner_id
+        if hasattr(params, 'partner'):
+            partner = self.env['res.partner'].sudo().search([('vat', '=', params.partner.vat), ('company_id', '=', company.id)])
+            if not partner:
+                partner = self.env['res.partner'].sudo().with_context({'no_vat_validation': True, 'active_system': 'oco'}).create({
+                    'is_company': True,
+                    'company_id': company.id,
+                    'name': params.partner.name,
+                    'vat': params.partner.vat,
+                    'email': params.partner.email,
+                    'mobile': params.partner.phone,
+                    'country_id': country and country.id or False,
+                    'state_id': state and state.id or False,
+                    'street': getattr(params.partner, 'address', '') or '',
+                    'zip': getattr(params.partner, 'zip', '') or '',
+                    'city': getattr(params.partner, 'city', '') or '',
+                })
+
+        acquirer = self.env['payment.acquirer']._get_acquirer(company=company, providers=providers, limit=1)
+        values = {
+            'acquirer_id': acquirer.id,
+            'partner_id': partner.id,
+            'amount': params.amount,
+            'currency_id': company.currency_id.id,
+            'company_id': company.id,
+            'state': 'draft',
+            'jetcheckout_api_ok': True,
+            'jetcheckout_api_hash': hash,
+            'jetcheckout_api_id': params.id,
+            'jetcheckout_api_method': method,
+            'jetcheckout_api_order': params.order.name,
+            'jetcheckout_api_card_return_url': params.url.card.result,
+            'jetcheckout_api_html': getattr(params, 'html', False) or False,
+            'jetcheckout_api_contact': getattr(params.partner, 'contact', False) or False,
+            'jetcheckout_date_expiration': getattr(params, 'expiration', False) or False,
+            'jetcheckout_campaign_name': getattr(params, 'campaign', False) or False,
+            'jetcheckout_ip_address': request.httprequest.remote_addr,
+        }
+
+        products = getattr(params.order, 'products', [])
+        if products:
+            product_ids = []
+            prods = self.env['product.product'].sudo()
+            for product in products:
+                prod = prods.search([
+                    #('type', '=', 'product'),
+                    ('type', '=', 'consu'),
+                    ('default_code', '=', product),
+                    '|', ('company_id', '=', company.id),
+                         ('company_id', '=', False)
+                ])
+                if not prod:
+                    prod = prods.create({
+                        #'type': 'product',
+                        'type': 'consu',
+                        'name': product.name,
+                        'default_code': product.code,
+                    })
+                product_ids.append((0, 0, {
+                    'product_id': prod.id,
+                    'name': product.name,
+                    'code': product.code,
+                    'qty': product.qty
+                }))
+            values.update({'jetcheckout_api_product_ids': product_ids})
+            #values.update({'jetcheckout_api_product': ','.join(list(map(lambda x: x.name, products)))})
+
+        if getattr(params.url, 'bank', None):
+            values.update({'jetcheckout_api_bank_return_url': params.url.bank.result})
+            if getattr(params.url.bank, 'webhook', None):
+                values.update({'jetcheckout_api_bank_webhook_url': params.url.bank.webhook})
+
+        tx = self.env['payment.transaction'].sudo().create(values)
+        tx.write({
+            'partner_name': params.partner.name,
+            'partner_vat': params.partner.vat,
+            'partner_email': params.partner.email,
+            'partner_phone': params.partner.phone,
+            'partner_address': getattr(params.partner, 'address', '') or '',
+            'partner_zip': getattr(params.partner, 'zip', '') or '',
+            'partner_city': getattr(params.partner, 'city', '') or '',
+            'partner_country_id': country and country.id or False,
+            'partner_state_id': state and state.id or False,
+        })
+
     def _get_acquirer(self, company):
         return self.env['payment.acquirer']._get_acquirer(company=company, providers=['jetcheckout'], limit=1, raise_exception=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def _get_oco(self, company, oco):
         domain = [('is_company', '=', True), ('company_id', '=', company.id)]
