9a7ae2932ae7ac44ef4c0032d79aaa8c47a89a4c
hasan.tayfur <fi3ei2@gmail.com>
2024-07-15 17:32:27 +0300

    [IMP] payment order checkout module

--- payment_oco/models/transaction.py
+++ payment_oco/models/transaction.py
@@ -19,11 +19,14 @@ class PaymentTransaction(models.Model):
                 'order_line': [(0, 0, {
                     'product_id': line.product_id.id,
                     'product_uom_qty': line.qty,
+                    'price_unit': line.price,
                 }) for line in self.jetcheckout_api_product_ids]
             })]
             self.sale_order_ids.onchange_partner_id()
             for line in self.sale_order_ids.order_line:
+                price = line.price_unit
                 line.product_id_change()
+                line.price_unit = price
 
     def _paylox_auth_postprocess(self):
         if self.company_id.system == 'oco':
