590b0588714b47d3212c7c525474efdc987c4688
Hasan Tayfur <fi3ei2@gmail.com>
2025-03-04 22:40:27 +0300

    [IMP] item add

--- payment_jetcheckout_system/migrations/1.26/pre-migrate.py
+++ payment_jetcheckout_system/migrations/1.26/pre-migrate.py
@@ -6,3 +6,5 @@ def migrate(cr, version):
         cr.execute('ALTER TABLE res_company ADD COLUMN payment_page_item_add_desc_numericonly boolean')
     if not column_exists(cr, 'res_company', 'payment_page_item_add_desc_unique'):
         cr.execute('ALTER TABLE res_company ADD COLUMN payment_page_item_add_desc_unique boolean')
+    if not column_exists(cr, 'res_company', 'payment_page_item_add_desc_required'):
+        cr.execute('ALTER TABLE res_company ADD COLUMN payment_page_item_add_desc_required boolean')
