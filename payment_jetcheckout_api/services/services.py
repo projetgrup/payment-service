eec1da342bf96723416271493ab237270d4630ca
hasan.tayfur <fi3ei2@gmail.com>
2025-03-14 14:28:39 +0300

    [IMP] api

--- payment_jetcheckout_api/services/services.py
+++ payment_jetcheckout_api/services/services.py
@@ -656,7 +656,7 @@ class PaymentAPIService(Component):
             return result
 
     def _get_transaction_from_hash(self, company, hash):
-        return self.env['payment.transaction'].sudo().search([('company_id', '=', company), ('jetcheckout_api_hash', '=', hash)], limit=1)
+        return self.env['payment.transaction'].sudo().search([('company_id', '=', company.id), ('jetcheckout_api_hash', '=', hash)], limit=1)
 
     def _get_transaction_from_token(self, token):
         return self.env['payment.transaction'].sudo().search([('jetcheckout_order_id', '=', token)], limit=1)
