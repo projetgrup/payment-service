a54f208c9dc331bac72c824c7ae01b6e2d85b39c
hasan.tayfur <fi3ei2@gmail.com>
2025-03-24 13:47:04 +0300

    [IMP] log

--- payment_jetcheckout/controllers/main.py
+++ payment_jetcheckout/controllers/main.py
@@ -573,7 +573,10 @@ class PayloxController(http.Controller):
 
     def _prepare_installment(self, acquirer=None, partner=0, amount=0, rate=0, currency=None, campaign='', bin='', token='', **kwargs):
         self._check_user()
-        loggable = self._log_state()
+        try:
+            loggable = self._log_state()
+        except:
+            loggable = None
         client = self._get_partner(partner, parent=True)
         if not request.env.user.has_group('base.group_user'):
             if client and client.campaign_id:
@@ -583,7 +586,7 @@ class PayloxController(http.Controller):
         transaction = self._get_transaction()
         currency =  self._get_currency(currency, acquirer)
         type = self._get_type()
-        url = '/prepayment/%sinstallment_options' % (acquirer._get_paylox_api_url(), bin and 'bin_' or '')
+        url = '/prepayment/%sinstallment_options' % (bin and 'bin_' or '',)
         data = {
             "application_key": acquirer.jetcheckout_api_key,
             "mode": acquirer._get_paylox_env(),
