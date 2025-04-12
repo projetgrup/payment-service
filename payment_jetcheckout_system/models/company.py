70e215b6a97fcd6a59f546a0c42ca9e425de5729
hasan.tayfur <fi3ei2@gmail.com>
2025-04-12 19:44:57 +0300

    [IMP] invoice on items

--- payment_jetcheckout_system/models/company.py
+++ payment_jetcheckout_system/models/company.py
@@ -24,6 +24,7 @@ class Company(models.Model):
     notif_sms_success_ok = fields.Boolean('Send Successful Payment Transaction SMS')
     notif_webhook_ids = fields.One2many('payment.settings.notification.webhook', 'company_id', 'Webhook URLs')
 
+    payment_einvoice_identifier = fields.Char('E-Invoice Identifier')
     payment_item_bank_token_ok = fields.Boolean('Link Bank Token and Payment Item')
     payment_dashboard_button_ok = fields.Boolean(string='Dashboard Payment Button', default=True)
     payment_dashboard_button_url = fields.Char(string='Dashboard Payment Button URL')
