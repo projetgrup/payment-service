03b98f66c20cbf6a2dd60959bfad9de8235122ce
hasan.tayfur <fi3ei2@gmail.com>
2025-06-27 17:14:25 +0300

    [IMP] sec audit

--- sec_audit/models/audit.py
+++ sec_audit/models/audit.py
@@ -199,31 +199,33 @@ class Audit(models.Model):
             @api.model_create_multi
             def create(self, vals_list, **kw):
                 records = create.origin(self, vals_list, **kw)
-                for record in records:
-                    log(self.env.cr, uid=self.env.uid, action='create', record=record)
+                if self.env.company.sec_audit_ok:
+                    for record in records:
+                        log(self.env.cr, uid=self.env.uid, action='create', record=record)
                 return records
             return create
 
         def make_write():
             def write(self, vals, **kw):
-                keys = []
-                values = {}
-                for key in vals.keys():
-                    if not self._fields[key].relational:
-                        keys.append(key)
-                if keys:
-                    self.env.cr.execute('''
-                        SELECT id,%s
-                        FROM %s
-                        WHERE id IN (%s)
-                    ''' % (','.join(keys), self._table, ','.join(map(str, self.ids))))
-                    res = self.env.cr.dictfetchall()
-                    for rec in res:
-                        values[rec['id']] = rec
+                if self.env.company.sec_audit_ok:
+                    keys = []
+                    values = {}
+                    for key in vals.keys():
+                        if not self._fields[key].relational:
+                            keys.append(key)
+                    if keys:
+                        self.env.cr.execute('''
+                            SELECT id,%s
+                            FROM %s
+                            WHERE id IN (%s)
+                        ''' % (','.join(keys), self._table, ','.join(map(str, self.ids))))
+                        res = self.env.cr.dictfetchall()
+                        for rec in res:
+                            values[rec['id']] = rec
 
                 write.origin(self, vals, **kw)
 
-                if values:
+                if self.env.company.sec_audit_ok and values:
                     for record in self:
                         tracking = []
                         if record.id in values:
@@ -234,7 +236,7 @@ class Audit(models.Model):
                                 field = self.env['ir.model.fields'].sudo()._get(self._name, f)
                                 if not field:
                                     continue
-                                if v != vals[f]:
+                                if f in vals and v != vals[f]:
                                     tracking.append((field.field_description, v, vals[f]))
                         if tracking:
                             tracking = json.dumps(tracking, default=str)
@@ -246,8 +248,9 @@ class Audit(models.Model):
 
         def make_unlink():
             def unlink(self, **kwargs):
-                for record in self:
-                    log(self.env.cr, uid=self.env.uid, action='unlink', record=record)
+                if self.env.company.sec_audit_ok:
+                    for record in self:
+                        log(self.env.cr, uid=self.env.uid, action='unlink', record=record)
                 return unlink.origin(self, **kwargs)
             return unlink
 
