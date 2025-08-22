9e44ad00703c1dc8bd4a9fb6f304a6b0ea683ff3
emrenuhak <emrenh.akbdk@gmail.com>
2025-08-22 09:58:38 +0300

    [IMP] Payment Escrow

--- payment_escrow/static/src/js/page.js
+++ payment_escrow/static/src/js/page.js
@@ -2332,7 +2332,7 @@ publicWidget.registry.payloxSystemEscrow = publicWidget.Widget.extend({
         const recipientType = $('input[name="recipientType"]:checked').val();
         
         const data = {
-            recipient_type: recipientType,
+            customer_type: recipientType,
         };
 
         // Add product_id if we're editing an existing ad or have a saved product
@@ -2344,25 +2344,25 @@ publicWidget.registry.payloxSystemEscrow = publicWidget.Widget.extend({
 
         if (recipientType === 'individual') {
             // Individual recipient data using field system
-            data.recipient_name_surname = this.recipient.input.name_surname.$.val();
-            data.recipient_identity = this.recipient.input.identity.$.val();
-            data.recipient_birthdate = this.recipient.input.birthdate.$.val();
-            data.recipient_phone = this.recipient.input.phone_individual.$.val();
-            data.recipient_email = this.recipient.input.email_individual.$.val();
-            data.recipient_address = this.recipient.input.address_individual.$.val();
+            data.customer_name_surname = this.recipient.input.name_surname.$.val();
+            data.customer_identity = this.recipient.input.identity.$.val();
+            data.customer_birthdate = this.recipient.input.birthdate.$.val();
+            data.customer_phone = this.recipient.input.phone_individual.$.val();
+            data.customer_email = this.recipient.input.email_individual.$.val();
+            data.customer_address = this.recipient.input.address_individual.$.val();
         } else {
             // Corporate recipient data using field system
-            data.recipient_corporate_title = this.recipient.input.corporate_title.$.val();
-            data.recipient_tax_number = this.recipient.input.tax_number.$.val();
-            data.recipient_tax_office = this.recipient.input.tax_office.$.val();
-            data.recipient_person = this.recipient.input.person.$.val();
-            data.recipient_phone = this.recipient.input.phone_corporate.$.val();
-            data.recipient_email = this.recipient.input.email_corporate.$.val();
-            data.recipient_address = this.recipient.input.address_corporate.$.val();
+            data.customer_corporate_title = this.recipient.input.corporate_title.$.val();
+            data.customer_tax_number = this.recipient.input.tax_number.$.val();
+            data.customer_tax_office = this.recipient.input.tax_office.$.val();
+            data.customer_person = this.recipient.input.person.$.val();
+            data.customer_phone = this.recipient.input.phone_corporate.$.val();
+            data.customer_email = this.recipient.input.email_corporate.$.val();
+            data.customer_address = this.recipient.input.address_corporate.$.val();
         }
 
         return this._rpc({
-            route: '/my/recipient/save',
+            route: '/my/customer/save',
             params: data,
         });
     },
