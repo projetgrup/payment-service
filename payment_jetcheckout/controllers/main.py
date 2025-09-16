ed76b5f910d462cbd94a847e81b9f115c30ed400
emrenuh <emrenh.akbdk@gmail.com>
2025-09-16 10:00:53 +0300

    [IMP] campaign

--- payment_jetcheckout/controllers/main.py
+++ payment_jetcheckout/controllers/main.py
@@ -134,13 +134,12 @@ class PayloxController(http.Controller):
     def _get_campaign(acquirer=None, partner=None, transaction=None):
         campaign = PayloxController._get('campaign')
         if not campaign:
-            if partner and partner.campaign_id:
-                campaign = partner.campaign_id.name
-                raise Exception(campaign)
-            elif acquirer and acquirer.jetcheckout_campaign_id:
+            if acquirer:
                 campaign = acquirer.jetcheckout_campaign_id.name
-            elif transaction and transaction.jetcheckout_campaign_name:
+            elif transaction:
                 campaign = transaction.jetcheckout_campaign_name
+            elif partner:
+                campaign = partner.campaign_id.name
             else:
                 campaign = ''
             PayloxController._set('campaign', campaign)
