f44f123d3b48cbf7dbf83c22a760cd8af78d560b
emrenuhak <emrenh.akbdk@gmail.com>
2025-10-10 15:00:28 +0300

    [FIX] Tax Office

--- payment_escrow/controllers/main.py
+++ payment_escrow/controllers/main.py
@@ -1723,6 +1723,7 @@ class PayloxSystemEscrowController(Controller):
                     'state_id': int(kwargs.get('state_id')),
                     'broker_sign_name': kwargs.get('sign_name'),
                     'broker_authorized_person': kwargs.get('person'),
+                    'tax_office': kwargs.get('tax_office', 'Merkez'),
                     'city': kwargs.get('city'),
                     'is_company': True,
                     'company_id': company.id,
