ead9eb45779d10d6da4e86408ecd3710b60fabda
emrenuhak <emrenh.akbdk@gmail.com>
2025-10-23 17:34:53 +0300

    [FIX] escrow

--- payment_escrow/controllers/main.py
+++ payment_escrow/controllers/main.py
@@ -420,13 +420,12 @@ class PayloxSystemEscrowController(Controller):
             dealer_rate = 0.0
             if broker_dealer and tx_campaign:
                 dealer_commission = broker_dealer.dealer_commission_rate_ids.filtered(lambda c: c.campaign_id.id == tx_campaign.id and c.active)
-                raise Exception(dealer_commission)
                 if dealer_commission:
                     dealer_rate = dealer_commission.commission_rate or 0.0
             
             if not additional_rates:
                 return result
-            
+            raise Exception(dealer_rate)
             currency_obj = request.env['res.currency'].sudo().browse(int(currency)) if currency else request.env.company.currency_id
             precision = currency_obj.decimal_places or 2
             amount_value = float(amount or 0.0)
