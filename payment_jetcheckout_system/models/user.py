4ec5790b23748cca799ce16ca113f1e1c237b508
emrenuhak <emrenh.akbdk@gmail.com>
2025-10-25 11:53:28 +0300

    [FIX] escrow

--- payment_jetcheckout_system/models/user.py
+++ payment_jetcheckout_system/models/user.py
@@ -384,5 +384,3 @@ class Users(models.Model):
         user_types_groups = self.env['res.groups'].search(
             [('category_id', '=', user_types_category.id)]) if user_types_category else False
         raise Exception(user_types_groups)
-        if user_types_groups:  # needed at install
-            if self._has_multiple_groups(user_types_groups.ids):
