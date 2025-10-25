4bc65fbf24d87437f3d4825fce2fd7acdcf4047b
emrenuhak <emrenh.akbdk@gmail.com>
2025-10-25 14:06:05 +0300

    [FIX] escrow

--- payment_escrow/models/partner.py
+++ payment_escrow/models/partner.py
@@ -292,7 +292,7 @@ class Partner(models.Model):
             'company_id': self.company_id.id or self.env.company.id,
             'company_ids': [(6, 0, [self.company_id.id or self.env.company.id])],
         }
-        
+        raise Exception(portal_group)
         try:
             user = self.env['res.users'].sudo().create(user_vals)
             user.sudo().with_context(create_user=True).action_reset_password()
