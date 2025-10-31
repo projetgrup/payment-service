346f12deecb1c1fe877e5404db6fe8edf5dfbb8e
hasan.tayfur <fi3ei2@gmail.com>
2025-10-31 14:27:01 +0300

    [IMP] escrow transaction page js

--- payment_escrow/static/src/js/broker_transactions.js
+++ payment_escrow/static/src/js/broker_transactions.js
@@ -2,11 +2,13 @@
 'use strict';
 
 import rpc from 'web.rpc';
-import { _t } from 'web.core';
+import { _t, qweb } from 'web.core';
+import dialog from 'web.Dialog';
 import publicWidget from 'web.public.widget';
 import payloxPage from 'paylox.page';
 import framework from 'paylox.framework';
 import fields from 'paylox.fields';
+import { format } from 'paylox.tools';
 
 
 publicWidget.registry.payloxBrokerTransaction = payloxPage.extend({
@@ -19,6 +21,9 @@ publicWidget.registry.payloxBrokerTransaction = payloxPage.extend({
     init: function (parent, options) {
         this._super(parent, options);
         this.transactions = {
+            uploadInvoice: new fields.element({
+                events: [['click', this._onClickUploadInvoice]]
+            }),
             invoiceFile: new fields.file({
                 name: 'invoiceFile',
                 allowMultiple: false,
@@ -54,6 +59,90 @@ publicWidget.registry.payloxBrokerTransaction = payloxPage.extend({
     //--------------------------------------------------------------------------
     // Handlers
     //--------------------------------------------------------------------------
+
+    _onClickUploadInvoice: function(ev) {
+        const id = parseInt(ev.currentTarget.dataset.id);
+        const transferAmount = parseFloat(ev.currentTarget.dataset.transferAmount);
+        const adNumber = ev.currentTarget.dataset.adNumber;
+        const currency = this.currency;
+        console.log(currency);
+        const popup = new dialog(this, {
+            title: _t('Upload Invoice'),
+            technical: false,
+            size: 'small',
+            dialogClass: 'o_payment_escrow_upload_invoice',
+            $content: qweb.render('paylox.escrow.transaction.uploadInvoice', {
+                format,
+                currency,
+                transferAmount,
+                adNumber,
+                id,
+            }),
+            buttons: [{
+                text: _t('Cancel'),
+                classes: 'btn-secondary text-white',
+                close: true,
+            }, {
+                text: _t('Upload'),
+                classes: 'btn-primary text-white',
+                click: () => {
+                    $button.find('.card-token-loading').addClass('show');
+                    $button.find('.card-token-radio-add').click();
+                    popup.destroy();
+                    rpc.query({
+                        route: '/payment/card/remove',
+                        params: { token: $input.val() },
+                    }).then((res) => {
+                        if (res.error) {
+                            self.displayNotification({
+                                type: 'danger',
+                                title: _t('Error'),
+                                message: _t('An error occured. Please contact with your system administrator.'),
+                            });
+                        } else {
+                            $button.remove();
+                        }
+                    }).guardedCatch((error) => {
+                        $button.find('.card-token-loading').removeClass('show');
+                        if (!ev?.detail?.noWarning) {
+                            self.displayNotification({
+                                type: 'danger',
+                                title: _t('Error'),
+                                message: _t('An error occured. Please contact with your system administrator.'),
+                            });
+                        }
+                    });
+                },
+            }],
+        });
+        popup.open().opened(() => {
+            const props = {
+                name: 'invoiceFile',
+                allowMultiple: false,
+                accept: 'image/*, application/pdf',
+                maxFileSize: '10MB',
+                className: 'escrow-wizard-file',
+                labelIdle: `<svg class="w-100" width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M31.4998 33.2514C30.5318 33.2514 29.7484 32.468 29.7484 31.5C29.7484 30.532 30.5318 29.7486 31.4998 29.7486C35.3594 29.7486 38.5012 26.6068 38.5012 22.7473C38.5012 19.0723 35.626 16.0084 31.9592 15.7705L30.9256 15.709L30.4867 14.7779C28.7559 11.1152 25.0316 8.74863 20.9998 8.74863C16.968 8.74863 13.2438 11.1152 11.5129 14.7779L11.074 15.709L10.0445 15.7746C6.37363 16.0125 3.50254 19.0764 3.50254 22.7514C3.50254 26.6109 6.64434 29.7527 10.5039 29.7527C11.4719 29.7527 12.2553 30.5361 12.2553 31.5041C12.2553 32.4721 11.4719 33.2555 10.5039 33.2555C4.7125 33.2555 0.00390625 28.5469 0.00390625 22.7555C0.00390625 17.5834 3.79785 13.2193 8.80996 12.4031C11.2709 8.02676 15.9508 5.25 20.9998 5.25C26.0488 5.25 30.7287 8.02676 33.1938 12.3949C38.2059 13.2111 41.9998 17.5793 41.9998 22.7514C41.9998 28.5387 37.2912 33.2514 31.4998 33.2514Z" fill="black"/>
+                        <path d="M26.2502 32.3737C25.8032 32.3737 25.3561 32.2014 25.0116 31.861L21.0002 27.8497L16.9889 31.861C16.3081 32.5459 15.1965 32.5459 14.5157 31.861C13.8307 31.176 13.8307 30.0686 14.5157 29.3877L19.7657 24.1377C20.4465 23.4528 21.5581 23.4528 22.2389 24.1377L27.4889 29.3877C28.1739 30.0727 28.1739 31.1801 27.4889 31.861C27.1444 32.2055 26.6973 32.3737 26.2502 32.3737Z" fill="#2414D8"/>
+                        <path d="M21.0004 39.375C20.0324 39.375 19.249 38.5916 19.249 37.6236V25.3764C19.249 24.4084 20.0324 23.625 21.0004 23.625C21.9684 23.625 22.7518 24.4084 22.7518 25.3764V37.6277C22.7518 38.5916 21.9684 39.375 21.0004 39.375Z" fill="#2414D8"/>
+                    </svg>
+                    <span class="text-600">Fatura Yükle</span>
+                    <div class="text-600">PDF veya Resim</div>`,
+                allowImageEdit: true,
+                credits: false,
+                captureMethod: 'environment',
+                allowFileSizeValidation: true,
+                labelMaxFileSizeExceeded: _t('File is too large'),
+                labelMaxFileSize: _t('Maximum file size is {filesize}'),
+                labelMaxTotalFileSizeExceeded: _t('Maximum total size exceeded'),
+                labelMaxTotalFileSize: _t('Maximum total file size is {filesize}'),
+            }
+
+            const $file = popup.$el.find('input');
+            FilePond.create($file[0], props);
+        });
+    },
 
     /**
      * Handle invoice file submission
