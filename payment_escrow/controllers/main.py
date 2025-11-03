21a7cbe5ef0ad3c7413f91bc1bdc3879c25ed30a
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-03 14:43:36 +0300

    [FIX] dealer

--- payment_escrow/controllers/main.py
+++ payment_escrow/controllers/main.py
@@ -2191,7 +2191,7 @@ class PayloxSystemEscrowController(Controller):
         dealer_referral_code = partner.dealer_referral_code if partner.dealer_referral_code else ''
         
         commission_rates = partner.dealer_commission_rate_ids.filtered('active')
-        
+        raise Exception(broker_volumes, broker_transactions)
         values = {
             'baskets': dealer_baskets,
             'brokers': brokers,
