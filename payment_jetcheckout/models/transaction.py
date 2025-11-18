a1523e031c491625be19f60af605a162d6a40d3c
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-18 11:33:30 +0300

    [FIX] approval_state

--- payment_jetcheckout/models/transaction.py
+++ payment_jetcheckout/models/transaction.py
@@ -1054,6 +1054,7 @@ class PaymentTransactionBasket(models.Model):
 
     def write(self, values):
         res = super().write(values)
+        raise Exception(values)
         if 'approval_state' in values:
             tx = fields.first(self).transaction_id
             if tx:
