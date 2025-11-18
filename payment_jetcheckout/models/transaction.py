08af2d7aec992502a7f291628803ec2cfe57c31d
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-18 12:36:40 +0300

    [FIX]

--- payment_jetcheckout/models/transaction.py
+++ payment_jetcheckout/models/transaction.py
@@ -1057,12 +1057,12 @@ class PaymentTransactionBasket(models.Model):
         if 'approval_state' in values:
             tx = fields.first(self).transaction_id
             if tx:
-                if all(t == '+' for t in tx.mapped('paylox_basket_ids.approval_state')):
+                if all(t == '+' for t in tx.sudo().mapped('paylox_basket_ids.approval_state')):
                     tx.write({
                         'jetcheckout_approval_state': '+',
                         'jetcheckout_approval_state_message': _('Approved'),
                     })
-                elif all(t == '-' for t in tx.mapped('paylox_basket_ids.approval_state')):
+                elif all(t == '-' for t in tx.sudo().mapped('paylox_basket_ids.approval_state')):
                     tx.write({
                         'jetcheckout_approval_state': '-',
                         'jetcheckout_approval_state_message': _('Disapproved'),
