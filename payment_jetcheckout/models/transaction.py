9d931edd4130f522593053b0d7031cfd5e4a45c2
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-18 13:37:05 +0300

    [IM] New ınsurance method

--- payment_jetcheckout/models/transaction.py
+++ payment_jetcheckout/models/transaction.py
@@ -1058,12 +1058,13 @@ class PaymentTransactionBasket(models.Model):
         if 'approval_state' in values:
             tx = fields.first(self).transaction_id
             if tx:
-                if all(t == '+' for t in tx.mapped('paylox_basket_ids.approval_state')):
+                _logger.info(tx.with_context(skip_approval_state_update=True).mapped('paylox_basket_ids.approval_state'))
+                if all(t == '+' for t in tx.with_context(skip_approval_state_update=True).mapped('paylox_basket_ids.approval_state')):
                     tx.write({
                         'jetcheckout_approval_state': '+',
                         'jetcheckout_approval_state_message': _('Approved'),
                     })
-                elif all(t == '-' for t in tx.mapped('paylox_basket_ids.approval_state')):
+                elif all(t == '-' for t in tx.with_context(skip_approval_state_update=True).mapped('paylox_basket_ids.approval_state')):
                     tx.write({
                         'jetcheckout_approval_state': '-',
                         'jetcheckout_approval_state_message': _('Disapproved'),
