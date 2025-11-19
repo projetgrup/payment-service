9f1566f0373c7dff07cf0bc0153138cd5f3988ab
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-19 13:49:36 +0300

    [FIX]

--- payment_jetcheckout/models/transaction.py
+++ payment_jetcheckout/models/transaction.py
@@ -1007,14 +1007,11 @@ class PaymentTransactionBasket(models.Model):
             try:
                 if response.status_code == 200:
                     result = response.json()
-                    _logger.error("Paylox Response for basket %s: %s", basket.id, result)
                     if result['response_code'] == "00":
-                        _logger.error("Writing approval state for basket %s", basket.id)
                         basket.with_context(skip_escrow_check_access_rule=True).sudo().write({
                             'approval_state': '+',
                             'approval_state_message': _('Approved'),
                         })
-                        _logger.error("Write successful for basket %s. New state: %s", basket.id, basket.approval_state)
                     else:
                         basket.with_context(skip_escrow_check_access_rule=True).sudo().write({
                             'approval_state_message': _('%s (Error Code: %s)') % (result['message'], result['response_code']),
