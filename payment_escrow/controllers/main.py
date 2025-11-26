b0d67d2c1f8846f94aad1213638971e32c6a5757
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-26 13:33:03 +0300

    [IMP] ad

--- payment_escrow/controllers/main.py
+++ payment_escrow/controllers/main.py
@@ -183,10 +183,8 @@ class PayloxSystemEscrowController(Controller):
             different = kwargs.get('different_holder', {}).get('different', False)
             if different:
                 partner = request.env['res.partner'].sudo().search([('vat', '=', kwargs.get('different_holder', {}).get('vat', '')), ('paylox_escrow_type', '=', 'card_holder')], limit=1)
-            ads = kwargs.get('ads', [])
-            raise Exception(ads)
-            payment_items = request.env['payment.item'].sudo().search([('ad_id', 'in', ads and [a['aid'] for a in ads] or [])])
-            raise Exception(payment_items)
+            products = kwargs.get('products', [])
+            payment_items = request.env['payment.item'].sudo().search([('ad_id', 'in', products and [p['pid'] for p in products] or [])])
             res.update({
                 'paylox_transaction_item_ids':[(0, 0, {
                         'item_id': rec.id,
