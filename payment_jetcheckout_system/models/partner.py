a7343ba40066176853183f18ab5df5fb5e45272f
emrenuhak <emrenh.akbdk@gmail.com>
2025-11-28 10:18:20 +0300

    [FIX] query auto

--- payment_jetcheckout_system/models/partner.py
+++ payment_jetcheckout_system/models/partner.py
@@ -113,25 +113,22 @@ class PartnerBank(models.Model):
         if 'api_merchant' in values:
             values['api_merchant'] = normalize(values['api_merchant'])
         res = super(PartnerBank, self).create(values)
-        if not self.env.context.get('import_file'):
-            res.action_api_save(mode='create')
-            res.action_api_query()
+        res.action_api_save(mode='create')
         return res
 
     def write(self, values):
         if 'api_merchant' in values:
             values['api_merchant'] = normalize(values['api_merchant'])
         res = super(PartnerBank, self).write(values)
-        if not self.env.context.get('import_file'):
-            if 'acc_number' in values or 'api_merchant' in values:
-                for bank in self:
-                    if bank.api_ref:
-                        bank.action_api_save(mode='update')
-                    else:
-                        bank.action_api_save(mode='create')
-                for bank in self:
-                    if not bank.api_state:
-                        bank.action_api_save()
+        if 'acc_number' in values or 'api_merchant' in values:
+            for bank in self:
+                if bank.api_ref:
+                    bank.action_api_save(mode='update')
+                else:
+                    bank.action_api_save(mode='create')
+            for bank in self:
+                if not bank.api_state:
+                    bank.action_api_save()
         return res
 
     def action_api_save(self, mode=None):
